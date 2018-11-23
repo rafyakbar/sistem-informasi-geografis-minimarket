d400c0e46097023a3f21290d7492cb3232d289f8
Unknown <rafy683@gmail.com>
2018-11-23 17:02:13 +0700

    untuk mysql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         setlocale(LC_TIME, 'Indonesia');
         Carbon::setLocale('id');
 
