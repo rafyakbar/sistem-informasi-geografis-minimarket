70c372998911b03ac57e92de1cbee90342b4a724
Unknown <rafy683@gmail.com>
2018-12-01 14:26:35 +0700

    getBarangTerlaris

--- app/Toko.php
+++ app/Toko.php
@@ -53,4 +53,36 @@ class Toko extends Model
         $data = $this->hasMany(Transaksi::class, 'toko_id');
         return $queryReturn ? $data : $data->get();
     }
+
+    /**
+     * Mendapatkan barang terlaris
+     * @param int $limit
+     * @return \Illuminate\Support\Collection
+     */
+    public function getBarangTerlaris($limit = 10)
+    {
+        $ids = DB::table('transaksi_produk')
+            ->select('produk_id')
+            ->whereIn('transaksi_id', function ($query) {
+                $query->select('id')
+                    ->from('transaksi')
+                    ->where('toko_id', $this->id);
+            })->groupBy('produk_id')
+            ->orderByRaw('sum(jumlah) desc')
+            ->get()
+            ->take($limit)
+            ->pluck('produk_id')
+            ->toArray();
+
+        $barangs = collect([]);
+        foreach ($ids as $produk_id){
+            $barangs->push(
+                $this->getProduk()
+                    ->wherePivot('id', $produk_id)
+                    ->first()
+            );
+        }
+
+        return $barangs;
+    }
 }
